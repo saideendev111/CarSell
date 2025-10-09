4d4ffd0a22cdb07738975bedd34039575962399c
MSaideen <mohammd.bassam203@gmail.com>
2025-10-09 17:35:45 +0300

    finished setting up datbase configuration

--- cars_resale_project/settings.py
+++ cars_resale_project/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'cars_resale_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cars_store',  # Name of your PostgreSQL database
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',  
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
