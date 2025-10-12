97ae1e60839730e26b8f9560a325853f3ba151dd
MSaideen <mohammd.bassam203@gmail.com>
2025-10-12 11:17:22 +0300

    Added admin superuser

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Car, Company, Contact
 
 # Register your models here.
+
+admin.site.register(Car)
+admin.site.register(Company)
+admin.site.register(Contact)
