fc02dd22fb6d23bd0e198151b7674feb65f96d0e
MSaideen <mohammd.bassam203@gmail.com>
2025-10-12 20:32:45 +0300

    Added base, homepage, and css styles for the base

--- main_app/forms.py
+++ main_app/forms.py
@@ -14,4 +14,4 @@ class CarForm(forms.ModelForm):
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
-        fields = ['name', 'phone_num']
+        fields = ['contact_name', 'phone_num']
