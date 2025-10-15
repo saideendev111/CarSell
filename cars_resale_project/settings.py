2aa589f98220a544e56e18bfbd8dd459ec8e2957
MSaideen <mohammd.bassam203@gmail.com>
2025-10-15 15:45:00 +0300

    fixed settings

--- cars_resale_project/settings.py
+++ cars_resale_project/settings.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 
 
 
-
+from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
